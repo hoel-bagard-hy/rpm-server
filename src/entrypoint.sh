46028af66a420d74f263c6d92061a89a888a69dc
Hoel Bagard <hoel.bagard.hy@hitachi-hightech.com>
2024-08-23 18:14:42 +0900

    make the setup scripts somewhat usable

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+if [ ! -d "/data/packages/el7-x86_64/repodata " ]; then
+    createrepo /data/packages/el7-x86_64/
+fi
+
 service nginx restart
 nginx
--- src/setup.sh
+++ src/setup.sh
@@ -1,10 +1,11 @@
 # The actual setup was done by entering the commands manually, but this script should still be useful in
 # case a re-setup is necessary.
 
-# First add the binaries from outside the docker.
+# Create a folder where the binaries/repo data will be stored. We could use a volume, but that way is slightly more convenient imho.
 mkdir /home/rpm
+# Initialise the repo with the binaries from the GitHub repo.
 cpr Metis_PackageRepo/rpm/el7-x86_64 /home/rpm
+# Remove the non-binary files.
 rm -r /home/rpm/el7-x86_64/repodata /home/rpm/el7-x86_64/Metis.repo
-
-# Then set-up the server from inside the docker.
-createrepo /data/packages/el7-x86_64/
+# That one file has different permissions from the rest, fix it.
+chmod +x /home/rpm/util-linux-2.23.2-61.el7_7.1.x86_64.rpm
