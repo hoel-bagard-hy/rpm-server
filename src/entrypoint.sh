3444ea376589f397425c25469e48f38530cc32b0
Hoel Bagard <hoel.bagard.hy@hitachi-hightech.com>
2024-08-26 16:46:06 +0900

    fix typo in path

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -d "/data/packages/el7-x86_64/repodata " ]; then
+if [ ! -d "/data/packages/el7-x86_64/repodata" ]; then
     createrepo /data/packages/el7-x86_64/
 fi
 
