0c5346f658a5f8388514980cd231e50bfde5e56f
Hoel Bagard <hoel.bagard.hy@hitachi-hightech.com>
2024-09-05 19:29:38 +0900

    update file structure (in src only)

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-if [ ! -d "/data/packages/el7-x86_64/repodata" ]; then
-    createrepo /data/packages/el7-x86_64/
+if [ ! -d "/data/packages/approved/el7-x86_64/repodata" ]; then
+    createrepo /data/packages/approved/el7-x86_64/
+fi
+
+if [ ! -d "/data/packages/approved/el9-x86_64/repodata" ]; then
+    createrepo /data/packages/approved/el9-x86_64/
+fi
+
+if [ ! -d "/data/packages/approval-pending/el7-x86_64/repodata" ]; then
+    createrepo /data/packages/approval-pending/el7-x86_64/
+fi
+
+if [ ! -d "/data/packages/approval-pending/el9-x86_64/repodata" ]; then
+    createrepo /data/packages/approval-pending/el9-x86_64/
 fi
 
 nginx -g 'daemon off;'
